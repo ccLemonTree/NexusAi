62bfcac6ee995683ed37a687e85cc78eb598431a
NexusAi <nexusai@project.com>
2026-07-07 13:46:10 +0800

    sam3: batch label_list in chunks of 8 for multi-label inference

--- api/infer/Triton_model/sam3/sam3_detector.py
+++ api/infer/Triton_model/sam3/sam3_detector.py
@@ -58,59 +58,59 @@ def sam3(triton_client, service_name, init_data, img, label_rules, box_info, con
 
     # --- 4. 构造 Prompt ---
     label_list_analyse = list(label_rules.keys()) if isinstance(label_rules, dict) else list(label_rules)
-    label_list = [i.replace("sam3_","") for i in label_list_analyse]
-    TEXT_PROMPTS = np.array([str(x).encode('utf-8') for x in label_list], dtype=object)
+    label_list = [i.replace("sam3_", "") for i in label_list_analyse]
 
-    # --- 5. 构造 Triton Inputs ---
+    # --- 5. 构造 Triton Inputs 配置 ---
     img_in_name = INPUT_CFG[0]["name"]
     txt_in_name = INPUT_CFG[1]["name"]
     img_type = INPUT_CFG[0]["data_type"].split("_")[-1]
     txt_type = INPUT_CFG[1]["data_type"].split("_")[-1]
-    if "STRING" in txt_type: txt_type = "BYTES"
-
-    inputs = [
-        grpcclient.InferInput(img_in_name, img_data.shape, img_type),
-        grpcclient.InferInput(txt_in_name, TEXT_PROMPTS.shape, txt_type)
-    ]
-    inputs[0].set_data_from_numpy(img_data)
-    inputs[1].set_data_from_numpy(TEXT_PROMPTS)
+    if "STRING" in txt_type:
+        txt_type = "BYTES"
 
     outputs = [grpcclient.InferRequestedOutput(obj["name"]) for obj in OUTPUT_CFG]
 
     pretreatment_end = time.time()  # [时间点2] 预处理结束
 
-    # --- 6. 推理 ---
-    output_dict = {}
-    if triton_client.is_model_ready(service_name):
-        results = triton_client.infer(service_name, inputs, outputs=outputs)
-        for obj in OUTPUT_CFG:
-            name = obj["name"]
-            data = results.as_numpy(name)
-            output_dict[name] = data
-
-    postprocessing_start = time.time()  # [时间点3] 后处理开始
-
-    # --- 7. 结果判空与后处理 ---
+    # --- 6. 分批推理（sam3 最多支持 8 个 label） ---
+    MAX_LABELS = 8
     detected_objects = []
+    model_ready = triton_client.is_model_ready(service_name)
 
-    if not output_dict:
-        postprocessing_end = time.time()
-        time_json["Pretreatment"] = pretreatment_end - pretreatment_start
-        time_json["post_time"] = postprocessing_start - pretreatment_end
-        time_json["processing_time"] = postprocessing_end - postprocessing_start
-        return detected_objects, time_json
+    for chunk_start in range(0, len(label_list), MAX_LABELS):
+        chunk_analyse = label_list_analyse[chunk_start:chunk_start + MAX_LABELS]
+        chunk_labels  = label_list[chunk_start:chunk_start + MAX_LABELS]
 
-    # 调用 postprocess 时传入 label_rules
-    detected_objects = postprocess(
-        output_dict,
-        orig_w,
-        orig_h,
-        final_conf_thres,
-        label_list_analyse,
-        label_rules=label_rules  # 传入规则字典
-    )
+        TEXT_PROMPTS = np.array([str(x).encode('utf-8') for x in chunk_labels], dtype=object)
 
-    postprocessing_end = time.time()  # [时间点4] 后处理结束
+        inputs = [
+            grpcclient.InferInput(img_in_name, img_data.shape, img_type),
+            grpcclient.InferInput(txt_in_name, TEXT_PROMPTS.shape, txt_type)
+        ]
+        inputs[0].set_data_from_numpy(img_data)
+        inputs[1].set_data_from_numpy(TEXT_PROMPTS)
+
+        if not model_ready:
+            continue
+
+        results = triton_client.infer(service_name, inputs, outputs=outputs)
+        output_dict = {obj["name"]: results.as_numpy(obj["name"]) for obj in OUTPUT_CFG}
+
+        if not output_dict:
+            continue
+
+        chunk_rules = {k: label_rules[k] for k in chunk_analyse} if isinstance(label_rules, dict) else chunk_analyse
+        detected_objects += postprocess(
+            output_dict,
+            orig_w,
+            orig_h,
+            final_conf_thres,
+            chunk_analyse,
+            label_rules=chunk_rules
+        )
+
+    postprocessing_start = time.time()
+    postprocessing_end = time.time()
 
     # --- 8. 时间计算 ---
     time_json["Pretreatment"] = pretreatment_end - pretreatment_start
